4f0d1ec1a3406e5034c2da965716570cb8fc512c
feroz khan <ferozk8417@gmail.com>
2020-06-22 10:56:12 -0700

    laravel escorts project

---
+++ asset/js/active.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+ 	$('.caroselAra.owl-carousel').owlCarousel({
+ 		items:8,
+	    loop:true,
+	    margin:10,
+	    autoplay:true,
+	    autoplayTimeout:2000,
+	    autoplayHoverPause:true,
+		responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	        },
+	        481:{
+	            items:2
+	        },
+	        600:{
+	            items:3,
+	        },
+	        1000:{
+	            items:8,
+	            loop:true,
+	        }
+	    }
+	});
+	$('.smallImage.owl-carousel').owlCarousel({
+		items:3,
+ 		loop:true,
+	    margin:5,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:3
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:3,
+	        }
+	    }
+	});
+ });
+
+
